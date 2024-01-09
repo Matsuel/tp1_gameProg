4d7c0a690b9e8c0b702fa44853a5f39cec10f2d0
Matsuel <matsuel0308@gmail.com>
2024-01-09 10:01:19 +0100

    collider and quit on collision

--- Assets/Scripts/ShipCollider.cs
+++ Assets/Scripts/ShipCollider.cs
@@ -2,10 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
 public class ShipCollider : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Collision detected");
+        if (other.gameObject.name== "Asteroides(Clone)")
+        {
+            Destroy(gameObject);
+            #if UNITY_EDITOR
+            EditorApplication.isPlaying = false;
+            #else
+            Application.Quit();
+            #endif
+        }
+
     }
 }
