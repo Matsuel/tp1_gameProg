b01b87ef70546de1ba8fbe1cc56addbd971a0494
DoctorPok <58393254+DoctorPok42@users.noreply.github.com>
2024-01-14 16:20:29 +0100

    Feat -> rocket : add boost

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    public int speed=10;
-    public float maxX=10.2f;
-    public float minX=-10.2f;
-    public float maxY=4.5f;
-    public float minY=-4.5f;
-    // Start is called before the first frame update
+    public int speed = 5;
+    public int boost = 2;
+    public float maxX = 10.2f;
+    public float minX = -10.2f;
+    public float maxY = 4.5f;
+    public float minY = -4.5f;
+
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -20,16 +21,24 @@ public class Movement : MonoBehaviour
     {
         float moveX = Input.GetAxis("Horizontal");
         float moveY = Input.GetAxis("Vertical");
-        
+
         Vector2 move = new Vector2(moveX, moveY);
 
         transform.Translate(move * Time.deltaTime* speed);
-        
-        Vector2 postition = transform.position;
+
 
         postition.x = Mathf.Clamp(postition.x, minX, maxX);
         postition.y = Mathf.Clamp(postition.y, minY, maxY);
+        Vector2 postition = transform.position;
 
-        transform.position = postition;
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            speed *= boost;
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            speed /= boost;
+        }
     }
 }
